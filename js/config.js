da207e16d097fd442cec2eeba30d05c8cfb95634
Sameer Korgawkar <sameer.kor1998@gmail.com>
2025-10-08 11:17:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_ZZ2IDkiWh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1clsvllom5g65jqhgo14lfo8gt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6qvr2nh39uel5sigr028mto2d9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
